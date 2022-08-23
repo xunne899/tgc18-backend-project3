166731bd30805f92555d88a504be34822ff5fb90
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-08-23 10:39:13 +0000

    added api products

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ app.use("/api/carts", express.json(), api.carts);
 app.use("/api/customers", express.json(), api.customers);
 app.use("/api/products", express.json(), api.products);
 app.use("/api/orders", express.json(), api.orders);
-app.use("/api/checkout", express.json(), api.orders);
+app.use("/api/checkout", express.json(), api.checkout);
 
 // app.use("/api/carts", express.json(), api.carts);
 

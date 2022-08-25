824a8e6b16183396860a8acd63a68d08c362f4ee
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-08-25 12:27:40 +0000

    Deploy to Heroku

--- bookshelf/index.js
+++ bookshelf/index.js
@@ -5,8 +5,8 @@ const knex = require('knex')({
       user: process.env.DB_USER,
       password:process.env.DB_PASSWORD,
       database:process.env.DB_DATABASE,
-      'host':process.env.DB_HOST,
-      'ssl': {
+      host:process.env.DB_HOST,
+      ssl: {
           'rejectUnauthorized': false
       }
 

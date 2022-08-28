eb5fde3eed98ea8f57139b54c481b74cb7612d8c
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-08-28 08:31:30 +0000

    modify api route for cart

--- index.js
+++ index.js
@@ -193,27 +193,18 @@ const FileStore = require("session-file-store")(session);
 const csrf = require("csurf");
 require("dotenv").config();
 
-
 const app = express();
-
 
 app.set("view engine", "hbs");
 
-
-
-
-
 app.use(express.static("public"));
-
 
 wax.on(hbs.handlebars);
 wax.setLayoutPath("./views/layouts");
 
 const PORT = process.env.PORT || 3000;
 
-
 app.use(cors());
-
 
 app.use(
   express.urlencoded({
@@ -221,22 +212,18 @@ app.use(
   })
 );
 
-
 app.use(
   session({
-    store: new FileStore(), 
-    secret: process.env.SESSION_SECRET, 
-    resave: false, 
-    saveUninitialized: true, 
+    store: new FileStore(),
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
   })
 );
 
-
 app.use(flash());
 
-
 app.use(function (req, res, next) {
-
   res.locals.success_messages = req.flash("success_messages");
   res.locals.error_messages = req.flash("error_messages");
   next();
@@ -251,10 +238,8 @@ app.use(function (req, res, next) {
   }
 });
 
-
 app.use(function (err, req, res, next) {
   if (err && err.code === "EBADCSRFTOKEN") {
-
     req.flash("error_messages", "The form has expired. Please try again");
 
     res.redirect("back");
@@ -263,21 +248,17 @@ app.use(function (err, req, res, next) {
   }
 });
 
-
 app.use(function (req, res, next) {
-
   if (req.csrfToken) {
     res.locals.csrfToken = req.csrfToken();
   }
   next();
 });
 
-
 app.use(function (req, res, next) {
   res.locals.user = req.session.user;
   next();
 });
-
 
 app.use(function (req, res, next) {
   res.locals.cloudinaryName = process.env.CLOUDINARY_NAME;
@@ -293,7 +274,7 @@ const cloudinaryPathRoutes = require("./routes/cloudinary");
 const loginPathRoutes = require("./routes/login");
 const orderPathRoutes = require("./routes/orders");
 
-const cartAPIPathRoutes = require("./routes/api/carts");
+const cartAPIPathRoutes = require("./routes/carts");
 const checkoutAPIPathRoutes = require("./routes/api/checkout");
 // const productAPIPathRoutes = require("./routes/api/products");
 // const orderAPIPathRoutes = require("./routes/api/orders");
@@ -321,7 +302,7 @@ app.use("/orders", [checkIfAuthenticated], orderPathRoutes);
 app.use("/checkout", checkoutAPIPathRoutes);
 app.use("/cart", [checkIfAuthenticated], cartAPIPathRoutes);
 
-app.use("/api/carts", express.json(),checkIfAuthenticatedJWT, api.carts);
+app.use("/api/carts", express.json(), checkIfAuthenticatedJWT, api.carts);
 app.use("/api/customers", express.json(), api.customers);
 app.use("/api/products", express.json(), api.products);
 app.use("/api/orders", express.json(), checkIfAuthenticatedJWT, api.orders);
--- routes/api/carts.js
+++ routes/api/carts.js
@@ -7,18 +7,13 @@ const router = express.Router();
 
 router.get("/", async function (req, res) {
   const cartItems = await cartServices.getCart(req.session.user.id);
-  res.render("cart/index", {
+  res.status(200);
+  res.json({
     cartItems: cartItems.toJSON(),
   });
-});
-
-router.get("/:variant_id/add", async function (req, res) {
-  const userId = req.session.user.id;
-  const variantId = req.params.variant_id;
-  const variantQty = 1;
-  await cartServices.addToCart(userId, variantId, variantQty);
-  req.flash("success_messages", "Item Added to cart successfully");
-  res.redirect("/cart/");
+  // res.render("cart/index", {
+  //   cartItems: cartItems.toJSON(),
+  // });
 });
 
 router.post("/:variant_id/add", async function (req, res) {
@@ -26,21 +21,10 @@ router.post("/:variant_id/add", async function (req, res) {
   const variantId = req.params.variant_id;
   const variantQty = req.body.quantity;
   await cartServices.addToCart(userId, variantId, variantQty);
-  req.flash("success_messages", "Item Added to cart successfully");
-  res.redirect("/cart/");
-});
-
-router.post("/:variant_id/update", async function (req, res) {
-  const userId = req.session.user.id;
-  const variantId = req.params.variant_id;
-  if (req.body.newQuantity > 0) {
-    await cartServices.updateQuantity(userId, variantId, req.body.newQuantity);
-    req.flash("success_messages", "New Quantity updated");
-    res.redirect("/cart");
-  } else {
-    req.flash("error_messages", "Quantity must be > 0");
-    res.redirect("/cart/");
-  }
+  res.status(200);
+  res.json({
+    statusMessage: "Successful",
+  });
 });
 
 router.put("/:variant_id/update", async function (req, res) {
@@ -48,24 +32,24 @@ router.put("/:variant_id/update", async function (req, res) {
   const variantId = req.params.variant_id;
   if (req.body.newQuantity > 0) {
     await cartServices.updateQuantity(userId, variantId, req.body.newQuantity);
-    req.flash("success_messages", "New Quantity updated");
-    res.redirect("/cart");
+    res.status(200);
+    res.json({
+      statusMessage: "Successful",
+    });
   } else {
-    req.flash("error_messages", "Quantity must be > 0");
-    res.redirect("/cart/");
+    res.status(400);
+    res.json({
+      statusMessage: "Quantity must be > 0",
+    });
   }
-});
-
-router.get("/:variant_id/delete", async function (req, res) {
-  await cartServices.removeCartItem(req.session.user.id, req.params.variant_id);
-  req.flash("success_messages", "Item has been removed from cart");
-  res.redirect("/cart/");
 });
 
 router.delete("/:variant_id/delete", async function (req, res) {
   await cartServices.removeCartItem(req.session.user.id, req.params.variant_id);
-  req.flash("success_messages", "Item has been removed from cart");
-  res.redirect("/cart/");
+  res.status(200);
+  res.json({
+    statusMessage: "Item has been removed from cart",
+  });
 });
 
 module.exports = router;

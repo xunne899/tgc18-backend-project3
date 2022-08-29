c897a70fb6d68d51ca7f2fdf63b40b393b876ba1
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-08-29 15:35:17 +0000

    modify orders route

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -2,15 +2,17 @@ const express = require("express");
 const router = express.Router();
 const { getOrderByCustomerId, getallOrders } = require("../../dal/orders");
 
-router.get('/:customer_id', async (req, res) => {
-    try {
-        const orders = await getOrderByCustomerId(req.params.customer_id)
-        res.send(orders)
-    } catch {
-        res.sendStatus(500)
-    }
-
-})
+router.get("/", async (req, res) => {
+  try {
+    const jwtInfo = req.customer; // comes from jwt processing
+    const userId = jwtInfo.id;
+    const orders = await getOrderByCustomerId(userId);
+    res.status(200);
+    res.json(orders);
+  } catch {
+    res.sendStatus(500);
+  }
+});
 
 // router.get("/", async (req, res) => {
 //   try {
